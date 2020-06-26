1fef711d937e3c40acd9030ca283fbf2f889352b
Emiliano <emi.perez997@gmail.com>
2020-06-26 13:37:40 -0300

    Frontend Terminado

--- js/main.js
+++ js/main.js
@@ -14,28 +14,68 @@ const deleteTask = (id) => {
 
 }
 
-const addTask = (title, desc) => {
-    if (title == "" || desc == ""){
-        alert("Ambos campos deben llenarse!");
-        document.querySelector(".title").value = ""
-        document.querySelector(".desc").value= ""
-        return;
-    }
-    // console.log(title, desc)
-    const xmlhttp = new XMLHttpRequest()
+const clearInputs = () => {
+    document.querySelector(".id").value = ""
+    document.querySelector(".title").value = ""
+    document.querySelector(".desc").value = ""
 
-    xmlhttp.onload = function (){
-        console.log(this.responseText)
-        document.querySelector(".title").value = ""
-        document.querySelector(".desc").value = ""
-        document.location.reload()
-    }
+}
 
+const addTask = (id, title, desc) => {
+
+    if(id != ""){
+        // Actualizar
+        //console.log(id, title, desc, "Actualizado")
+
+        const xmlhttp = new XMLHttpRequest()
+
+        xmlhttp.onload = function (){
+            console.log(this.responseText)
+            clearInputs()
+            document.location.reload()
+        }
+
+        
+        xmlhttp.open("POST", "backend/Main.php")
+        // Sin esto no recibe el parametro por metodo POST
+        xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+        xmlhttp.send(`id=${id}&&title=${title}&&desc=${desc}`)
+    }else{
+        // Agregar
+        if (title == "" || desc == ""){
+            alert("Ambos campos deben llenarse!");
+            clearInputs()
+            return;
+        }
+        // console.log(title, desc)
+        const xmlhttp = new XMLHttpRequest()
+
+        xmlhttp.onload = function (){
+            console.log(this.responseText)
+            clearInputs()
+            document.location.reload()
+        }
+
+        
+        xmlhttp.open("POST", "backend/Main.php")
+        // Sin esto no recibe el parametro por metodo POST
+        xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+        xmlhttp.send(`title=${title}&&desc=${desc}`)
+    }
     
-    xmlhttp.open("POST", "backend/Main.php")
-    // Sin esto no recibe el parametro por metodo POST
-    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-    xmlhttp.send(`title=${title}&&desc=${desc}`)
+
+}
+
+const editTask = (id, title, desc) => {
+    let inputId = document.querySelector('.id')
+    let inputTitle =  document.querySelector('.title')
+    let inputDesc = document.querySelector('.desc')
+
+    inputId.value = id
+    inputTitle.value = title
+    inputDesc.value = desc
+
+    console.log(id, title, desc)
 
 }
 
@@ -47,9 +87,9 @@ const showTasks = () => {
         let tabla = document.querySelector("#datos")
         const datos = JSON.parse(this.responseText)
         // console.log(datos)
-        datos.forEach( tarea => {
+        datos.forEach(tarea => {
             let tr = document.createElement('tr')
-            tr.innerHTML = `<td> ${tarea.title} </td> <td> ${tarea.description} </td> <td> <button class="btn btn-danger" onclick="deleteTask(${tarea.id})"> <i class="material-icons">delete</i> </button>  <button class="btn btn-success" onclick="editTask(${tarea.id}, ${tarea.title}, ${tarea.description})"><i class="material-icons">edit</i></button> </td> `
+            tr.innerHTML = `<td> ${tarea.title} </td> <td> ${tarea.description} </td> <td> <button class="btn btn-danger" onclick="deleteTask(${tarea.id})"> <i class="material-icons">delete</i> </button>  <button class="btn btn-success" onclick="editTask(${tarea.id}, '${tarea.title}', '${tarea.description}')"><i class="material-icons">edit</i></button> </td> `
             tabla.appendChild(tr)
 
         })
